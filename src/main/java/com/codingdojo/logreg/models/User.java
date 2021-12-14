af446d3ee57c7c1fec0afc630b1b350979d6bac3
theo <91856536+tkaleel@users.noreply.github.com>
2021-12-13 19:34:25 -0800

    Update User.java

--- src/main/java/com/codingdojo/logreg/models/User.java
+++ src/main/java/com/codingdojo/logreg/models/User.java
@@ -1,14 +1,21 @@
 package com.codingdojo.logreg.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="users")
@@ -34,6 +41,34 @@ public class User {
 	@NotEmpty(message="Confirm Password is required!")
 	@Size(min=8, max=100, message="Confirm Password must be between 8 and 100 characters.")
 	private String confirm;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	@PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 	
 	public User() {}
 
